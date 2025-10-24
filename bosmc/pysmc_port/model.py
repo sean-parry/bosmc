46e636b75d8623b0d5cea792955e2f8f3b5549a5
sean-parry <seancameronparry@gmail.com>
2025-10-24 12:20:23 +0100

    pysmc push

---
+++ bosmc/pysmc_port/model.py
@@ -0,0 +1,26 @@
+# ok so i need a log pdf and a log grad pdf probably targeting gp torch nad then setting the hypereparams of the botorch model see how mcmc works 
+import numpy as np
+import numpy.typing as npt
+
+# gpytorch
+def model():
+
+
+
+
+class target():
+
+    def logpdf(
+            X: npt.,
+            phi
+    ):
+
+    return
+
+    def logpdfgrad()
+
+
+
+
+if __name__ == '__main__':
+        return
